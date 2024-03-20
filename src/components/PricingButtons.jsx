531740adc4dde324bc8b172b5d73e61095655746
Shreyas Thakare <shreyasthakare5@gmail.com>
2024-03-20 13:36:11 +0530

    [ST] 20/3/24 Button

--- src/components/PricingButtons.jsx
+++ src/components/PricingButtons.jsx
@@ -2,9 +2,18 @@
 
 import React from 'react'
 
-const PricingButtons = ({ text }) => {
+const PricingButtons = ({ text, color }) => {
+
+    let bgcolor;
+
+    if (color) {
+        bgcolor = "#fff"
+    } else {
+        bgcolor = "#F8BF3B"
+    }
+
     return (
-        <button className='rounded-[40px] px-8 py-4 text-lg bg-[#F8BF3B] text-[#2C3D68] font-bold'>{text}</button>
+        <button className={`rounded-[40px] px-8 py-4 text-lg ${bgcolor} text-[#2C3D68] font-bold`}>{text}</button>
     )
 }
 
