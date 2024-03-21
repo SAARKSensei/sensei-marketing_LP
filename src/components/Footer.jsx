526fcbf9041d11c083bcbde824ef358e559e8a21
Shreyas Thakare <shreyasthakare5@gmail.com>
2024-03-21 12:09:49 +0530

    [ST] social media links, animation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import SenseiLogo from "../Images/Main logo.svg"
+import Link from 'next/link';
 
 const Footer = () => {
 
@@ -16,33 +17,37 @@ const Footer = () => {
                         alt='senseilogo'
                         className="h-40 w-40"
                     />
-                    <div className="text-sm text-slate-200">
+                    <div className="text-base font-NunitoSans font-normal text-slate-200">
                         <h4 className="mb-4">96659 52556</h4>
                         <h5>connect@sensei.org.in</h5>
                     </div>
                 </div>
                 <div>
-                    <h1 className="text-3xl text-white font-semibold w-44  mt-10">
+                    <h1 className="text-4xl font-NunitoSans text-white font-bold w-44 mt-10">
                         Life-Skills Education
                     </h1>
                 </div>
                 <div>
                     <div className="flex flex-row gap-44 text-sm leading-7 mt-10 ml-44 font-light">
-                        <div className="text-[#FF8B13] ">
+                        <div className="text-[#FF8B13] font-NunitoSans font-semibold font-base">
                             <h2>About</h2>
                             <h2>Growers</h2>
                             <h2>Merchants</h2>
                             <h2>Partners</h2>
                             <h2>Contact</h2>
                         </div>
-                        <div className="text-white">
+                        <div className="text-white font-NunitoSans font-normal text-sm">
                             <h2>Facebook</h2>
                             <h2>Twitter</h2>
-                            <h2>Linkedin</h2>
-                            <h2>Instagram</h2>
+                            <Link href={"https://www.linkedin.com/company/mr-sensei/?viewAsMember=true"}>
+                                <h2>Linkedin</h2>
+                            </Link>
+                            <Link href={"https://www.instagram.com/the_sensei_community/"}>
+                                <h2>Instagram</h2>
+                            </Link>
                         </div>
                     </div>
-                    <p className="text-white mt-8 font-extralight ml-44">
+                    <p className="font-NunitoSans font-normal text-sm text-white mt-8 ml-44">
                         © {year} SAARK Edu. Pvt. Ltd. All rights reserved.
                     </p>
                 </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,23 +4,28 @@ import Image from 'next/image'
 import React from 'react'
 import SenseiLogo from "../Images/Main logo.svg"
 import Button from './Button'
+import Link from 'next/link'
 
 const Navbar = () => {
     return (
         <div className='flex items-center justify-between absolute top-10 w-3/4 left-0 right-0 mx-auto bg-[#2C3D68] rounded-[40px] pl-6 p-2 z-50'>
-            <Image
-                src={SenseiLogo}
-                alt='Sensei logo'
-                className='w-28 h-14'
-            />
+            <Link href={"https://sensei.org.in/"}>
+                <Image
+                    src={SenseiLogo}
+                    alt='Sensei logo'
+                    className='w-28 h-14 cursor-pointer'
+                />
+            </Link>
             <div className='flex gap-10 font-Nunito text-lg'>
-                <p className=''>About Us</p>
+                <Link href={"https://sensei.org.in/about-us/"}>
+                    <p className=''>About Us</p>
+                </Link>
                 <p className=''>Parent</p>
                 <p className=''>Why Us</p>
                 <p className=''>Curriculum</p>
             </div>
             <div className='flex gap-3 h-[59px] items-center font-Nunito'>
-                <button className='rounded-[40px] px-8 py-4 border-2 border-[#F8BF3B] text-[#F8BF3B] text-xl font-bold'>Login</button>
+                <button className='hidden rounded-[40px] px-8 py-4 border-2 border-[#F8BF3B] text-[#F8BF3B] text-xl font-bold'>Login</button>
                 <Button text={"Buy NOW"} />
             </div>
         </div>
